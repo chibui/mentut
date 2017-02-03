5a3f369a2348036485a0848850bf1d3e6a313922
chi <chi_bui@outlook.com>
2017-02-03 11:09:52 +1100

    add dependancies

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // es6
 
+// load enviromental variables
+require('dotenv').config();
+
 // grab dependencies
-const express = require('express'),
-  app = express(),
-  port = process.env.PORT || 8080;
-  expressLayouts = require('express-ejs-layouts');
+const express     = require('express'),
+  app             = express(),
+  port            = process.env.PORT || 8080;
+  expressLayouts  = require('express-ejs-layouts');
+  mongoose        = require('mongoose');
 
 // configure app
 // tell express where to look for static assets
@@ -13,6 +17,9 @@ app.use(express.static(__dirname + '/public'));
 // set ejs as templating engine
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
+
+// connect to db
+mongoose.connect(process.env.DATABASE);
 
 // set routes
 app.use(require('./app/routes'));
