56bf293b0e172d22c3d61e76f23982c05d43322d
chi <chi_bui@outlook.com>
2017-02-03 12:08:23 +1100

    connect db and refactor

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -1,25 +1,44 @@
 const Event = require('../models/event');
 
 module.exports = {
+  showEvents: showEvents,
+  showSingle: showSingle,
+  seedEvents: seedEvents
+}
+// show all events
+function showEvents(req, res) {
+// get all events
 
-  // show all events
-  showEvents: (req, res) => {
-    // create dummy events
-    const events = [
-      { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.'},
-      { name: 'Swimming', slug: 'swimming', description: 'In a pool'},
-      { name: 'Lifting', slug: 'lifting', description: 'Pick up heavy things'},
+
+// return a view with data
+res.render('pages/events', {events: events });
+}
+
+// show a single event
+function showSingle(req, res) {
+// get a single event
+const event = { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.'};
+res.render('pages/single', {event: event });
+}
+
+// seed our db
+function seedEvents(req, res) {
+// create some events
+const events = [
+      { name: 'Basketball', description: 'Throwing into a basket.'},
+      { name: 'Swimming', description: 'In a pool'},
+      { name: 'Lifting', description: 'Pick up heavy things'},
+      { name: 'Kung Fu', description: 'Punching and kicking'}
     ];
 
-    // return a view with data
-    res.render('pages/events', {events: events });
-  },
-
-  // show a single event
-  showSingle: (req, res) => {
-    // get a single event
-    const event = { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.'};
-    res.render('pages/single', {event: event });
+// use the Event model to add/save
+Event.remove({}, () => {
+  for (event of events) {
+    var newEvent = new Event(event);
+    newEvent.save();
   }
+});
 
-};
+// seeded
+res.send('Database seeded!');
+}
